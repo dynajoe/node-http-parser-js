679e05b5f356b445f6ba7fe268cf6f7c940c206b
Joe Andaverde <joe@andaverde.net>
2012-07-28 01:13:24 -0500

    Before working on chunked encoding

--- HttpParser.js
+++ HttpParser.js
@@ -16,19 +16,22 @@ for (k in states) {
 var CR = 0xD;
 var LF = 0xA;
 
-var requestExpression = /(GET|POST|PUT|DELETE) (.*) HTTP\/1.(1|0)/;
-var responseExpression = /HTTP\/1.(1|0) (\d{3}) (.*)/;
+var requestExpression = /^([A-Z]+) (.*) HTTP\/(\d)\.(\d)$/i;
+var responseExpression = /^HTTP\/(\d)\.(\d) (\d{3}) (.*)$/i;
 
 HTTPParser.REQUEST = "REQUEST";
 HTTPParser.RESPONSE = "RESPONSE";
 
-var Request = function () {
+var Info = function () {
    this.versionMajor = 1;
    this.versionMinor = 1;
    this.statusCode = 200;
    this.method = undefined;
    this.url = undefined;
    this.headers = [];
+   this.shouldKeepAlive = true;
+   this.upgrade = false;
+   this.httpVersion = null;
 }
 
 function HTTPParser(type) {
@@ -38,8 +41,9 @@ function HTTPParser(type) {
       this.state = states.PARSING_RESPONSE;
    }
 
+   this.type = type;
    this.current_line = [];
-   this.request = new Request();
+   this.info = new Info();
    
    this.onHeaders = function (headers, url) {};
    this.onHeadersComplete = function (info) {};
@@ -48,12 +52,18 @@ function HTTPParser(type) {
 }
 
 HTTPParser.prototype.reinitialize = function (type) {
-   this.state = states.PARSING_REQUEST;
+   if (type == HTTPParser.REQUEST) {
+      this.state = states.PARSING_REQUEST;
+   } else {
+      this.state = states.PARSING_RESPONSE;
+   }
+
+   this.type = type;
    this.current_line = [];
-   this.request = new Request();
+   this.info = new Info();
 }
 
-HTTPParser.prototype.finish = function () { return 0; }
+HTTPParser.prototype.finish = function () { }
 
 HTTPParser.prototype.execute = function (data, offset, length) {
    var i = offset;
@@ -64,7 +74,6 @@ HTTPParser.prototype.execute = function (data, offset, length) {
       var c = data[i];
 
       if (this.state == states.PARSING_BODY) {
-         console.log(this.request);
          this.onBody(data, i, max - i);
          break;
       }
@@ -72,7 +81,7 @@ HTTPParser.prototype.execute = function (data, offset, length) {
       if (c == LF) {
          if (this.current_line.length == 0) {
             this.state = states.PARSING_BODY;
-            this.onHeadersComplete(this.request);
+            this.onHeadersComplete(this.info);
             this.onMessageComplete();
          } else {
             var line = this.current_line.join("");
@@ -90,7 +99,6 @@ HTTPParser.prototype.execute = function (data, offset, length) {
             this.state = states.PARSING_HEADER;
             this.current_line = [];
          }
-
          continue;
       } else if (c == CR) {
          continue;
@@ -98,23 +106,65 @@ HTTPParser.prototype.execute = function (data, offset, length) {
 
       this.current_line.push(String.fromCharCode(c));
    }
+
+   return length;
 }
 
 HTTPParser.prototype._parseResponseLine = function (line) {
    var match = responseExpression.exec(line);
-   this.request.versionMinor = match[1];
-   this.request.statusCode = match[2];
+   this.info.versionMajor = match[1]
+   this.info.versionMinor = match[2];
+   this.info.statusCode = match[3];
+   this.info.shouldKeepAlive = this._shouldKeepAlive();
+}
+
+HTTPParser.prototype._shouldKeepAlive = function () {
+
+   if (this.info.versionMajor > 0 && this.info.versionMinor > 0) {
+      // if (parser->flags & F_CONNECTION_CLOSE) {
+      //    return 0;
+      // }
+   } else {
+      // /* HTTP/1.0 or earlier */
+      // if (!(parser->flags & F_CONNECTION_KEEP_ALIVE)) {
+      // return 0;
+      // }
+   }
+
+   return !this._messageNeedsEOF();
+}
+
+HTTPParser.prototype._messageNeedsEOF = function () {
+
+   if (this.type == HTTPParser.REQUEST) {
+      return false;
+   }
+
+   if (this.info.statusCode / 100 == 1 ||
+       this.info.statusCode == 204 || 
+       this.info.statusCode == 304) {
+      // || parser->flags & F_SKIP_BODY
+      return false;
+   }
+
+   // if ((parser->flags & F_CHUNKED) || parser->content_length != ULLONG_MAX) {
+   //    return 0;
+   // }
+
+   return true;
 }
 
 HTTPParser.prototype._parseRequestLine = function (line) {
    var match = requestExpression.exec(line);
-   this.request.method = match[1];
-   this.request.url = match[2];
-   this.request.versionMinor = match[3];
+   this.info.method = match[1];
+   this.info.url = match[2];
+   this.info.versionMajor = match[3];
+   this.info.versionMinor = match[4];
+   this.info.shouldKeepAlive = false;
 }
 
 HTTPParser.prototype._parseHeaderLine = function (line) {
    var lastIndex = line.indexOf(":");
-   this.request.headers.push(line.substring(0, lastIndex));
-   this.request.headers.push(line.substring(lastIndex + 1).trim());
+   this.info.headers.push(line.substring(0, lastIndex));
+   this.info.headers.push(line.substring(lastIndex + 1).trim());
 }
